f8fbab482fbb226680003a35061ed7716f2394f4
Arnaud <arnaud.belliard@crea-inseec.com>
2024-06-19 16:32:10 +0200

    finish

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,23 +1,38 @@
-import { useState } from "react"; import useTopic from "usetopic";
+"use client";
+import { useState } from "react";
+import useTopic from "usetopic";
+import './../style/chat.css'
 
-const MQTT_SERVER = "mqtt://test.mosquitto.org:8080";
+const MQTT_SERVER = "ws://test.mosquitto.org:8080";
 const TOPIC = "crea";
 
-
 const Chat = () => {
-    const { messages, sendMessage } = useTopic(MQTT_SERVER, TOPIC);
-    const [value, setValue] = useState("");
-    const send = () => { sendMessage(value); setValue(""); };
-    return (
-        <div>
-            <h1>Chat</h1>
-            {messages.map((message, index) => (
-            <div key={index}>{message}</div>
-            ))}
-            <input value={value} onChange={e => setValue(e.target.value)} />
-            <button onClick={send}>Send</button>
-        </div>
-     );
+  const { messages, sendMessage } = useTopic(MQTT_SERVER, TOPIC);
+  const [value, setValue] = useState("");
+
+  const send = () => {
+    sendMessage(value);
+    setValue("");
+  };
+
+  return (
+    <div>
+      <div className="message-box">
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+      <div className="message-input">
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Type your message here..."
+        />
+        <button onClick={send}>Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
